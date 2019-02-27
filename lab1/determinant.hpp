38612ce8677df0774f7bc937e6203f7bd217b91c
Michelle <miaovaberry@gmail.com>
2019-02-27 18:25:34 +0000

    removed older projects

--- lab1/determinant.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef DETERMINANT_HPP
-#define DETERMINANT_HPP
-
-//determinant declaration
-int determinant(int** array, int size); //function prototype
-
-#endif
--- lab1/readMatrix.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef READMATRIX_HPP
-#define READMATRIX_HPP
-
-//readMatric function declaration
-void readMatrix(int **arrayPtr, int size);  //function prototype
-
-
-#endif
